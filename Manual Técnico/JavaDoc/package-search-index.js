adb20b4fbc19f8d77025af16bfe609db20e138f9
Luis Sánchez <wichis6661@gmail.com>
2021-11-10 11:00:23 -0600

    Documentacion Agregada

---
+++ Manual Técnico/JavaDoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"datos"},{"l":"enums"},{"l":"manejadores"},{"l":"producciones"},{"l":"tablaSimbolos"},{"l":"token"},{"l":"ventanas"}]
